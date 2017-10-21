863ffb91803ac341f2e3771a284de2b3f066f840
bcameron1489 <bcameron1489@gmail.com>
2017-10-21 15:04:53 -0400

    Add commit function changes pick another pick another card added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,23 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!"); 
+  if (cardsInPlay.length <= 1) {
+	console.log("Pick another card");
   
-  } else { 
-	console.log("Sorry, try again.");
+  } else if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) { 
+	console.log("It's a Match!");
+  
+  } else {
+ 	console.log("Sorry, try again.");
+ 
   }
 }
 
 var flipCard = function (cardId) {
-	checkForMatch();
+	
 	cardsInPlay.push(cards[cardId])
 	console.log("User flipped" + ' ' + cards[cardId]);
+	checkForMatch();
 }
 
 flipCard(0)
